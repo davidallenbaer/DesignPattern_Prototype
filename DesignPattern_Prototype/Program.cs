1d3445afc9616b1e64bdc7dcce4a12c6291a49f6
davidallenbaer <davidallenbaer@gmail.com>
2021-05-18 22:45:17 -0400

    Add project files.

---
+++ DesignPattern_Prototype/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Prototype
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Name = "Dave";
+            emp1.Department = "IT";
+
+            Employee emp2 = emp1.GetClone();
+            emp2.Name = "Tom";
+
+            Console.WriteLine("Emplpyee 1: ");
+            Console.WriteLine("Name: " + emp1.Name + ", Department: " + emp1.Department);
+            Console.WriteLine("Emplpyee 2: ");
+            Console.WriteLine("Name: " + emp2.Name + ", Department: " + emp2.Department);
+
+            Console.Read();
+        }
+    }
+}
